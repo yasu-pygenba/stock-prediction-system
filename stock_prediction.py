67a84d0c1aad7ce3898019c5de95ff3d661d0a64
Yasu <genba.py.8yasu@gmai.com>
2026-07-16 21:35:39 +0900

    feat: .envを使用した環境変数とconfig.pyのリスト内の追加

--- stock_prediction.py
+++ stock_prediction.py
@@ -285,6 +285,111 @@ class DataPreprocessor:
 
         return merged_df
     
+# ３．特徴量生成
+class FeatureEngineer:
+
+    def __init__(self):
+        pass
+
+    def feature_create(self, clean_df: pd.DataFrame) -> pd.DataFrame:
+
+        df = clean_df.copy()
+
+        # 前日終値
+        df["前日終値"] = df.groupby("Code")["終値"].shift(1)
+        df["前日始値"] = df.groupby("Code")["始値"].shift(1)
+
+        # MAの計算
+        df["MA5"] = (
+            df.groupby("Code")["前日終値"]
+            .transform(lambda x: x.rolling(5).mean())
+        )
+
+        df["MA25"] = (
+            df.groupby("Code")["前日終値"]
+            .transform(lambda x: x.rolling(25).mean())
+        )
+
+        df["STD25"] = (
+            df.groupby("Code")["前日終値"]
+            .transform(lambda x: x.rolling(25).std(ddof=0))
+        )
+
+        for i in range(1, 4):
+            df[f"+{i}σ"] = df["MA25"] + df["STD25"] * i
+            df[f"-{i}σ"] = df["MA25"] - df["STD25"] * i
+
+        
+        # GU/GD他の計算
+        df["前日比寄り率"] = (
+            (df["始値"] - df["前日終値"])
+            / df["前日終値"] * 100
+        )
+
+        df["GU_GD"] = np.select(
+            [
+                df["始値"] >= df["前日終値"] * 1.01,
+                df["始値"] <= df["前日終値"] * 0.99,
+            ],
+            ["GU", "GD"],
+            default="レンジ"
+        )
+
+        df["MA5向き"] = (
+            df.groupby("Code")["MA5"]
+            .diff()
+            .apply(lambda x: "上向き" if x > 0 else "下向き" if x < 0 else None)
+        )
+
+        df["MA25向き"] = (
+            df.groupby("Code")["MA25"]
+            .diff()
+            .apply(lambda x: "上向き" if x > 0 else "下向き" if x < 0 else None)
+        )
+
+        df["前日終値_MA5判定"] = np.select(
+            [
+                df["前日終値"] > df["MA5"],
+                df["前日終値"] < df["MA5"],
+            ],
+            ["MA5より上", "MA5より下"],
+            default=None
+        )
+
+        df["前日終値_MA25判定"] = np.select(
+            [
+                df["前日終値"] > df["MA25"],
+                df["前日終値"] < df["MA25"],
+            ],
+            ["MA25より上", "MA25より下"],
+            default=None
+        )
+
+        df["前日終値_MA5乖離率"] = (
+            (df["前日終値"] - df["MA5"])
+            / df["MA5"] * 100
+        )
+
+        df["前日終値_MA25乖離率"] = (
+            (df["前日終値"] - df["MA25"])
+            / df["MA25"] * 100
+        )
+
+        df["引買_寄売_損益額_1株"] = df["始値"] - df["前日終値"]
+        df["引買_寄売_損益率"] = (
+            df["引買_寄売_損益額_1株"] / df["前日終値"] * 100
+        )
+
+        df["寄買_引売_損益額_1株"] = df["終値"] - df["始値"]
+        df["寄買_引売_損益率"] = (
+            df["寄買_引売_損益額_1株"] / df["始値"] * 100
+        )
+        
+        feature_df = df.copy()
+
+        return feature_df
+
+    
 
 
 
@@ -306,7 +411,11 @@ if __name__ == "__main__":
     prprocessor = DataPreprocessor(stock_df, index_df)
     clean_df = prprocessor.process()
 
-    print("\n --- 前処理後データの確認 ---")
-    print("前処理完了後の形状：", clean_df.shape)
+    # ----- 3.特徴量生成 -----------------------------------------------------
+    feature = FeatureEngineer()
+    feature_df = feature.feature_create(clean_df)
 
+    print("特徴量生成後の確認↓↓", feature_df.head(3))
+
+    
     print("\n テストおわり\n")
